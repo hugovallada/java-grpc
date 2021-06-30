68abcc2d4009c795b7dcc24f1804fc7d04f9b9be
hugovallada <valladahugo@gmail.com>
2021-06-30 13:08:21 -0300

    Implements unary client

--- src/main/java/com/github/hugovallada/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/github/hugovallada/grpc/greeting/client/GreetingClient.java
@@ -1,6 +1,9 @@
 package com.github.hugovallada.grpc.greeting.client;
 
 import com.proto.dummy.DummyServiceGrpc;
+import com.proto.greet.GreetRequest;
+import com.proto.greet.GreetServiceGrpc;
+import com.proto.greet.Greeting;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -9,11 +12,15 @@ public class GreetingClient {
     public static void main(String[] args) {
         System.out.println("Hello I'm a gRPC client");
 
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051).build();
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
 
         System.out.println("Ceating stub");
 
-        DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
+        var greetClient = GreetServiceGrpc.newBlockingStub(channel);
+
+
+        var response = greetClient.greet(GreetRequest.newBuilder().setGreeting(Greeting.newBuilder().setFirstName("Hugo").setLastName("Lopes").build()).build());
+        System.out.println(response.getResult());
 
         System.out.println("Shutting down channel");
         channel.shutdown();
